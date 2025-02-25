55caf78649695b0cca07b4e8bf17753fcd78a6d9
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2025-02-25 11:59:55 +0300

    sets

--- data-structures/script.js
+++ data-structures/script.js
@@ -220,3 +220,20 @@ console.log(new Set('Erhan'));
 
 console.log(orderSet.has('Pizza'));
 console.log(orderSet.has('Sushi'));
+orderSet.add('Garlic Bread');
+orderSet.add('Garlic Bread');
+orderSet.delete('Risotto');
+//orderSet.clear();
+console.log(orderSet);
+
+for (const order of orderSet) console.log(order);
+
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+);
+
+console.log(new Set('ErhanBulut').size);
